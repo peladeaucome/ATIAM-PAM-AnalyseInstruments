02f9c71a4221a3ac50a5576833a8bc7be9acf06a
peladeaucome <peladeaucome@orange.fr>
2023-01-13 15:38:11 +0100

    [dev][HN:preprocessing] Started to implement the QMF filter bank for preprocessing

--- HN_decomposition/Subspace/preprocessing/pseudo_QMF.py
+++ HN_decomposition/Subspace/preprocessing/pseudo_QMF.py
@@ -1,23 +1,22 @@
 import numpy as np
 import scipy.signal as sig
 
-def filter_bands(filterLength : int, numChannels : int, inputSignal : np.array):
+def filter_n_decimate(filterLength_smp : int, numBands : int, inputSignal : np.array):
     """
     args :
         - filterLength : [int]
             Digital filter length, in samples
-        - numChannels : [int]
+        - numBands : [int]
             Number of frequency channels in which the signal is to be separated
         - inputSignal : [(1, N) np.array]
             Input signal 
     returns :
-        - outputSignals : [numChannels, N) np.array]
+        - outputSignals : [numBands, N) np.array]
             Output signal"""
+    signalLength_smp = np.shape(inputSignal) #Input signal length
+    outputSignal = np.zeros((signalLength_smp, numBands))
 
-
-
-
-
-
+    for i in range(numBands):
+        outputSignal[:,i] = inputSignal
 
     return (outputSignal)
