3a7f2f00f0211111aa46ff55d858acbd4628f808
peladeaucome <peladeaucome@orange.fr>
2023-01-13 14:55:32 +0100

    small changes

---
+++ HN_decomposition/Subspace/pseudo_QMF.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def filter_bands(filterLength : int, numChannels : int, inputSignal : np.array):
+    """
+    args :
+        - filterLength : [int]
+            Digital filter length, in samples
+        - numChannels : [int]
+            Number of frequency channels in which the signal is to be separated
+        - inputSignal : [(1, N) np.array]
+            Input signal 
+    returns :
+        - outputSignals : [numChannels, N) np.array]
+            Output signal"""
+
+
+
+
+
+
+
+    return (outputSignal)
