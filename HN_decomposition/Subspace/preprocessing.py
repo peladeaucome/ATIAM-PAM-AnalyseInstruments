c660d53e722aba8988192c1afb5eaca5fd1c0180
peladeaucome <peladeaucome@orange.fr>
2023-01-17 15:03:50 +0100

    [HR][preprocessing] Cleaned the noise whitening process

--- HN_decomposition/Subspace/preprocessing.py
+++ HN_decomposition/Subspace/preprocessing.py
@@ -3,7 +3,7 @@ import librosa
 import scipy.signal as sig
 import scipy.linalg
 
-def rankFilter_stft(x_psd, rankFilter_bins, rankFilter_rank = .5):
+def rankFilter_stft(x_stft, rankFilter_bins, rankFilter_rank = .5):
     """
     Returns the noise PSD estimation
     args :
@@ -18,14 +18,41 @@ def rankFilter_stft(x_psd, rankFilter_bins, rankFilter_rank = .5):
         - X_filt : array-like
             STFT of the estimated noise
     """
-    rank_filtered = np.zeros(np.shape(x_psd))
+    x_psd = np.square(np.abs(x_stft))
+    rank_filtered_stft = np.zeros(np.shape(x_psd))
     for f in range(np.shape(x_psd)[0]):
         f_lowerBound = max(0, f-rankFilter_bins//2)
         f_upperBound = min(np.shape(x_psd)[0]-1, f+rankFilter_bins//2)
-        rank_filtered[f,:] = np.quantile(x_psd[f_lowerBound:f_upperBound], q = rankFilter_rank, axis = 0)
-    return rank_filtered
+        rank_filtered_stft[f,:] = np.quantile(x_psd[f_lowerBound:f_upperBound], q = rankFilter_rank, axis = 0)
+    return rank_filtered_stft
 
-def whiten_spectrum(x, n_fft, rankFilter_bins, rankFilter_rank, ARFilter_length, threshold = 1e-6):
+
+def compute_ARFilter(noise_psd, ARFilter_length):
+    """
+    Computes the auto-regressive filter coefficients to match a given noise power spectral density,
+    based on the assumption that the noise is obtained by filtering white noise with an AR filter.
+    args :
+        - noise_psd : array-like
+            power spectral density of the noise.
+        - ARFilter_length : int
+        number of coefficients of the AR filter.
+    
+    returns :
+        - ARFilter_a : array-like
+            Coeffeicients of the AR filter (the initial '1' is added so that this function is suitable to be used 
+            as-is with the scipy.signal.lfilter function : scipy.signal.lfilter([1], ARFilter_a, x)).
+    """
+    # Computing the autocorrelation vector
+    noise_autocorr_vec = np.fft.irfft(noise_psd)[:ARFilter_length+1]
+    noise_autocorr_vec /= noise_autocorr_vec[0]
+    # Autocorrelation vector
+    r = noise_autocorr_vec[1: ARFilter_length+1]
+    # Solving the Yule-Walker equations to get the AR filter coefficients
+    ARFilter_a = np.concatenate((np.ones(1), -scipy.linalg.solve_toeplitz(noise_autocorr_vec[:ARFilter_length], r)))
+    #ARFilter_a = np.concatenate((np.ones(1), -np.dot(scipy.linalg.inv(R), r)))
+    return ARFilter_a
+
+def whiten_stft(x, n_fft, rankFilter_bins, rankFilter_rank, ARFilter_length, threshold = 1e-6):
     """
     Whitens each window of x
     args :
@@ -46,9 +73,14 @@ def whiten_spectrum(x, n_fft, rankFilter_bins, rankFilter_rank, ARFilter_length,
         - xWhitened : array-like, same size as x
             x which has been 'whitened' on each window
     """
-    x_stft = librosa.stft(x, n_fft = n_fft, hop_length=n_fft, center=False)
-    x_stft_rankFiltered = rankFilter_stft(
-        x_psd = np.square(np.abs(x_stft)),
+    x_stft = librosa.stft(
+        x,
+        n_fft = n_fft,
+        hop_length=n_fft,
+        center=False
+    )
+    noise_psd = rankFilter_stft(
+        x_stft = x_stft,
         rankFilter_bins = rankFilter_bins,
         rankFilter_rank = rankFilter_rank
     )
@@ -58,15 +90,9 @@ def whiten_spectrum(x, n_fft, rankFilter_bins, rankFilter_rank, ARFilter_length,
     for t in range(np.shape(x_stft)[1]):
         x_windowed = x[t*n_fft:(t+1)*n_fft]
         if np.std(x_windowed)>threshold:
-            # Computing the autocorrelation vector
-            noise_autocorr_vec = np.fft.irfft(x_stft_rankFiltered[:,t])[:ARFilter_length+1]
-            # Autocorrelation matrix to solve the Yule-Walker Equation
-            R = scipy.linalg.toeplitz(noise_autocorr_vec[:ARFilter_length])
-            r = noise_autocorr_vec[1: ARFilter_length+1]
-            # Solving the Yule-Walker equations to get the AR filter coefficients
-            AR_filt = np.concatenate((np.ones(1), -np.dot(scipy.linalg.inv(R), r)))
+            ARFilter_a = compute_ARFilter(noise_psd[:,t], ARFilter_length)
             #Filtering x
-            xWhitened[t*n_fft:(t+1)*n_fft] = sig.lfilter(AR_filt, [1], x_windowed)
+            xWhitened[t*n_fft:(t+1)*n_fft] = sig.lfilter(ARFilter_a, [1], x_windowed)
         else:
             xWhitened[t*n_fft:(t+1)*n_fft] = x_windowed
     return xWhitened
