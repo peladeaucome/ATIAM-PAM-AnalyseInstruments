27bc2fa9d018ebbf94d67c9cd0642cfc11b2ef55
Armand Schwarz <armand.schwarz@free.fr>
2023-01-31 13:52:10 +0100

    Plaque_acier

--- Data/corde.py
+++ Data/corde.py
@@ -1,13 +1,17 @@
-def parametre_corde(article = False, acier = False):
+###c'est chaud entre ce qu'on a mesuré,  les bails de l'article et le reste
+def parametre_corde(article = False, acier_1 = False):
     if article:
-        T = 73.9 - 0.5
+        T = 73.9 
         rho_l = (3.61) * 10**(-3)
         Lc = 0.65
         r = 0.40 * 10e-4
         B =  4 * 10**(-5) 
-    if acier: 
-        T= 12
-
+    if acier_1: 
+        T = 87.27
+        rho_l = 4.80e-5 ##bizare
+        Lc = 0.65
+        r = 0.28 * 1e-4
+        B = 1 #jsp
     return(T,rho_l,Lc,r,B)
 
     
