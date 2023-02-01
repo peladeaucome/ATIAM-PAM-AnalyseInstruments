26e8ec7b6be58df4a53c8304f747a2fcc19e37b4
quentin-le-gall <quentin.legall@atiam.fr>
2023-02-01 17:30:40 +0100

    [ORGA] Mise à jour git classif

---
+++ Classification/A000_SVM/configs/config.py
@@ -0,0 +1,54 @@
+from pydantic import BaseModel
+import yaml
+import os
+
+
+class SVM(BaseModel):
+    feature_used: list
+    kernel_svm: str = 'rbf'
+    gamma_svm: float
+    C_svm: float
+    model_name: str
+    plot_title: str
+
+
+    
+class Dataset(BaseModel):
+    dataset_type: str = "list"
+    fs: int = 40000
+    resample: bool = True
+    resample_rate: int = 32768
+    valid_ratio: float = 0.1
+    
+
+class Train(BaseModel):
+    increment: int = 10
+
+    
+
+class MainConfig(BaseModel):
+    model: SVM = None
+    train: Train = None
+    dataset: Dataset = None
+
+
+def load_config(yaml_filepath="config.yaml"):
+    with open(yaml_filepath, "r") as config_f:
+        try:
+            config_dict = yaml.safe_load(config_f)
+            model_dict = {
+                "model": SVM(**config_dict["model"]),
+                "train": Train(**config_dict["train"]),
+                "dataset": Dataset(**config_dict["dataset"])
+
+            }
+            main_config = MainConfig(**model_dict)
+            return main_config
+        
+        except yaml.YAMLError as e:
+            print(e)
+
+
+def save_config(main_config, config_name="train_test_config.yaml"):
+    with open(os.path.join(config_name), 'w') as s:
+        yaml.safe_dump(main_config.dict(), stream=s)
