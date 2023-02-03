148682ee058539feda666505057b1bcef4762cea
adamulysse <91116745+adamulysse@users.noreply.github.com>
2023-02-03 14:02:19 +0100

    cartographie et plectre

---
+++ Data/.ipynb_checkpoints/Variation-checkpoint.py
@@ -0,0 +1,28 @@
+## variation des paramètres en pourcentage
+
+###variation de corde : 
+
+T_delta =  0.01
+
+rho_delta = 0.01
+
+Lc_delta =  0.01
+
+r_delta = 0.01
+
+B_delta =  0.01
+
+E_delta = 0.01
+
+### variation de table : 
+
+masseT_delta = 0.01
+
+L_xdelta = 0.01
+
+L_ydelta = 0.01 
+
+h_delta = 0.01
+
+E_nu_delta = 0.01
+
