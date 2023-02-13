63961c157d3589b7782a916e0990413626d8bec4
Armand Schwarz <armand.schwarz@free.fr>
2023-02-13 17:37:03 +0100

    new_amortissement_med_1

--- Data/table_corrigé_2.py
+++ Data/table_corrigé_2.py
@@ -2,16 +2,15 @@ import numpy as np
 
 def table(medium_1=False,medium_2=False,metal=False,plexi = False):
     if medium_1 : 
-        masseT = 236e-3
+        rho_T = 677.9661016949153
         L_x = 0.3793939393939394
         L_y = 0.30048101265822785 #largeur de la table (m)
         h = 2.95e-3 
         E_nu = 3206458380.6503973
-        xinB = np.array([0.08874667, 0.03074233, 0.05392   , 0.11241739, 0.05244179,
-        0.05397483, 0.06034859, 0.06737623, 0.01312974])
+        xinB = np.array([0.088747,0.053920,0.112417,0.052442,0.053975,0.013130,0.013130,0.061390,0.037500])
 
     if medium_2 :
-        masseT = 507e-3 #masse
+        rho_T = 711.3592995846896
         L_x = 0.40 
         L_y = 0.259  #largeur de la table (m)
         h = 6.04e-3 
@@ -20,7 +19,7 @@ def table(medium_1=False,medium_2=False,metal=False,plexi = False):
         0.05724961, 0.05446982, 0.04318039, 0.05932118])
 
     if metal : 
-        masseT = 1588e-3 #masse
+        rho_T = 2623.244373467073
         L_x = 39.5e-2 #longueur de la table (m)
         L_y = 25.5e-2 #largeur de la table (m)
         h = 6.01e-3 #épaisseur de la table (m)       
@@ -29,7 +28,7 @@ def table(medium_1=False,medium_2=False,metal=False,plexi = False):
         0.02160338, 0.04902268, 0.04280855, 0.04109246])
 
     if plexi :
-        masseT = 382e-3 #masse 
+        rho_T =  814.4294729660583 
         L_x = 40e-2 #longueur de la table (m)
         L_y = 25.9e-2 #largeur de la table (m)
         h = 4.51e-3 #épaisseur de la table (m)
@@ -37,4 +36,4 @@ def table(medium_1=False,medium_2=False,metal=False,plexi = False):
         xinB = np.array([0.07150503, 0.03790774, 0.07357441, 0.03845619, 0.04045318,
         0.01324686, 0.03842954, 0.02031206, 0.02085583])
 
-    return(masseT,L_x,L_y,h,E_nu,xinB)
+    return(rho_T,L_x,L_y,h,E_nu,xinB)
